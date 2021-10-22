da50a7a8361853af15da451877b29c163d45d891
jamesnathanan <artofgems@live.com>
2021-10-22 16:57:24 +0700

    fix boundaries issue

--- legacy-sketch-03.js
+++ legacy-sketch-03.js
@@ -1,6 +1,5 @@
 const canvasSketch = require("canvas-sketch");
 const random = require("canvas-sketch-util/random");
-const math = require("canvas-sketch-util/math");
 
 const settings = {
   dimensions: [1080, 1080],
@@ -12,35 +11,16 @@ const sketch = ({ context, width, height }) => {
   // const colors = ["#c2c2c2", "#ffffff", "#000000"];
   const colors = ["#264653", "#2a9d8f", "#e9c46a", "#f4a261", "#e76f51"];
 
-  for (let i = 0; i < 40; i++) {
+  for (let i = 0; i < 1000; i++) {
     const x = random.range(0, width);
     const y = random.range(0, height);
-    // const color = colors[Math.floor(Math.random() * colors.length)];
-    const color = "white";
+    const color = colors[Math.floor(Math.random() * colors.length)];
     agents.push(new Agent(x, y, color));
   }
 
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
-
-    for (let i = 0; i < agents.length; i++) {
-      const agent = agents[i];
-
-      for (let j = i + 1; j < agents.length; j++) {
-        const other = agents[j];
-
-        const dist = agent.pos.getDistance(other.pos);
-
-        if (dist > 200) continue; // continue means skip what written below
-        context.lineWidth = math.mapRange(dist, 0, 200, 12, 1); // if dist = 0 , linWdith = 12 || if dist = 200, linewidth = 1
-
-        context.beginPath();
-        context.moveTo(agent.pos.x, agent.pos.y);
-        context.lineTo(other.pos.x, other.pos.y);
-        context.stroke();
-      }
-    }
 
     agents.forEach((agent) => {
       agent.update();
@@ -57,25 +37,19 @@ class Vector {
     this.x = x;
     this.y = y;
   }
-
-  getDistance(v) {
-    const dx = this.x - v.x;
-    const dy = this.y - v.y;
-    return Math.sqrt(dx * dx + dy * dy);
-  }
 }
 
 class Agent {
   constructor(x, y, color) {
     this.pos = new Vector(x, y);
     this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
-    this.radius = random.range(4, 12);
+    this.radius = random.range(10, 20);
     this.color = color;
   }
 
   bounce(width, height) {
-    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1;
-    if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1;
+    if (this.pos.x <= 0 || this.pos.x >= width / 2) this.vel.x *= -1; //i dunno why put /2 is fix the right and bottom bounce issue
+    if (this.pos.y <= 0 || this.pos.y >= height / 2) this.vel.y *= -1; //i dunno why put /2 is fix the right and bottom bounce issue
   }
 
   update() {
@@ -89,12 +63,12 @@ class Agent {
     context.save();
     context.translate(this.pos.x, this.pos.y);
 
-    context.lineWidth = 4;
+    // context.lineWidth = 4;
 
     context.beginPath();
     context.arc(0, 0, this.radius, 0, Math.PI * 2);
     context.fill();
-    context.stroke();
+    // context.stroke();
 
     context.restore();
   }
